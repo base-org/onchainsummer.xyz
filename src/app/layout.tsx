904c5d4d13f2cefb98af28db2a9f3384a4fd4ff1
lauchness <lauchlanchisholm@gmail.com>
2023-05-30 16:23:40 -0300

    feat: better page structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +16,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="flex flex-col h-full">
+      <body className={clsx(inter.className, 'flex flex-col h-full')}>
+        <nav className="flex">
+          <ul className="flex gap-4">
+            <li>
+              <Link href={'/'}>Big NAV</Link>
+            </li>
+            <li>
+              <Link href={'sub'}>Sub</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/app/sub/page.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next'
+
+const Page: NextPage = () => {
+  return (
+    <main>
+      <h1>Sub Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora ipsum
+        quibusdam velit vero obcaecati. Ut at non quis minus eaque, placeat
+        nulla quia earum, labore sint odio fugit aut voluptate! Soluta placeat
+        nisi dolorem ratione laudantium culpa odio atque accusantium autem quis
+        vel eos quidem fuga velit officiis, voluptatum hic tenetur similique ad
+      </p>
+    </main>
+  )
+}
+
+export default Page
