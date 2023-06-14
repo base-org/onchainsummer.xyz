c8f7ff7510e1f46441cc18f520d652b4a9bcd124
Djordje <djordje_v@hotmail.com>
2023-06-14 16:41:13 -0700

    feat: finish first pass of trending desktop page

--- src/app/trending/page.tsx
+++ src/app/trending/page.tsx
@@ -1,203 +1,89 @@
-import { mock } from 'node:test'
+import { trending } from '@/config/trending'
 import Image from 'next/image'
-const mockTrendingData = [
-  {
-    id: 1,
-    title: 'Bear Market Builders',
-    subTitle: 'Bearbuilder.eth - 500 mints last hour - 0.0004 ETH',
-    images: [
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-    ],
-  },
-  {
-    id: 2,
-    title: 'Bear Market Builders',
-    subTitle: 'Bearbuilder.eth - 500 mints last hour - 0.0004 ETH',
-    images: [
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-    ],
-  },
-  {
-    id: 3,
-    title: 'Bear Market Builders',
-    subTitle: 'Bearbuilder.eth - 500 mints last hour - 0.0004 ETH',
-    images: [
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-    ],
-  },
-  {
-    id: 4,
-    title: 'Bear Market Builders',
-    subTitle: 'Bearbuilder.eth - 500 mints last hour - 0.0004 ETH',
-    images: [
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-    ],
-  },
-  {
-    id: 5,
-    title: 'Bear Market Builders',
-    subTitle: 'Bearbuilder.eth - 500 mints last hour - 0.0004 ETH',
-    images: [
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-    ],
-  },
-  {
-    id: 6,
-    title: 'Bear Market Builders',
-    subTitle: 'Bearbuilder.eth - 500 mints last hour - 0.0004 ETH',
-    images: [
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-    ],
-  },
-  {
-    id: 7,
-    title: 'Bear Market Builders',
-    subTitle: 'Bearbuilder.eth - 500 mints last hour - 0.0004 ETH',
-    images: [
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-      'https://picsum.photos/200/200',
-    ],
-  },
-]
+import { Button } from '@/components/Button'
 
 export default function Trending() {
   return (
     <main>
-      <div className="max-w-screen-xl mx-auto">
-        <div className="flex justify-between items-center bg-red-400 w-full">
+      <div className="max-w-screen-xl mx-auto mb-16">
+        <div className="flex justify-between items-center w-full relative top-4">
           <div>
-            <h1>Trending on Base this summer</h1>
+            <h1 className="font-medium text-[40px] leading-[50px]">
+              Trending on Base{' '}
+              <span className="text-transparent bg-clip-text bg-trending-linear-gradient">
+                this summer
+              </span>
+            </h1>
           </div>
           <div>
             <div>
-              <p className="text-neutral-500 text-lg">
-                Join the party. Create on Base
+              <p className="text-neutral-500 text-lg mb-4">
+                Join the party. Create on Base.
               </p>
               <div className="flex">
-                <div className="w-12 h-12 ml-1 mx-3 bg-black rounded-full"></div>
-                <div className="w-12 h-12 mx-3 bg-black rounded-full"></div>
-                <div className="w-12 h-12 mx-3 bg-black rounded-full"></div>
-                <div className="w-12 h-12 mr-1 mx-3 bg-black rounded-full"></div>
+                <Image
+                  src="/trending/crypto-icon-1.png "
+                  alt="Image Alt"
+                  width={48}
+                  height={48}
+                  priority
+                  className="mr-6"
+                />
+                <Image
+                  src="/trending/crypto-icon-2.png "
+                  alt="Image Alt"
+                  width={48}
+                  height={48}
+                  priority
+                  className="mr-6"
+                />
+                <Image
+                  src="/trending/crypto-icon-1.png "
+                  alt="Image Alt"
+                  width={48}
+                  height={48}
+                  priority
+                  className="mr-6"
+                />
+                <Image
+                  src="/trending/crypto-icon-2.png"
+                  alt="Image Alt"
+                  width={48}
+                  height={48}
+                  priority
+                />
               </div>
             </div>
           </div>
         </div>
-        <div className="border-t border-neutral-300 w-full my-10"></div>
-        <div className="px-8 py-10 border border-neutral-300 rounded-2xl">
-          {mockTrendingData.map(({ id, title, subTitle, images }) => (
-            <div key={id} className="flex w-full my-10 first:mt-0 last:mb-0">
-              <div>
-                <p>#{id}</p>
+        <div className="relative top-11 border-t border-neutral-300 w-full"></div>
+        <div className="bg-trending-linear-gradient-image bg-cover bg-no-repeat h-20"></div>
+        <div className="pl-8 pr-[60px] py-10 border border-neutral-300 z-50 rounded-2xl shadow-trending-card">
+          {trending.map(({ id, title, subTitle, images }) => (
+            <div key={id} className="flex w-full mb-[72px] last:mb-0">
+              <div className="mr-[29px]">
+                <p className="text-neutral-400">#{id}</p>
               </div>
               <div className="flex flex-col w-full">
-                <div className="flex justify-between">
+                <div className="flex justify-between mb-8">
                   <div>
                     <h3>{title}</h3>
-                    <p>{subTitle}</p>
+                    <p className="text-neutral-600">{subTitle}</p>
                   </div>
                   <div>
-                    <button>View More</button>
+                    <Button variant="SECONDARY">View More</Button>
                   </div>
                 </div>
                 <div className="flex">
                   {images.map((image, index) => (
-                    <div key={index} className="mx-2">
+                    <div key={index} className="mx-2 first:ml-0 last:mr-0">
                       <Image
                         src={image}
                         alt="Image Alt"
-                        width={100}
-                        height={100}
+                        width={65}
+                        height={65}
                         priority
+                        className="rounded-xl"
                       />
                     </div>
                   ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,17 @@ module.exports = {
         card: '6px 6px 0px #010101',
         button:
           '0px 1px 2px rgba(0, 0, 0, 0.12), 0px 0px 0px rgba(0, 0, 0, 0.08)',
+        'trending-card': '0px 20px 30px rgba(0, 0, 0, 0.04);',
       },
       fontFamily: {
         sans: ['var(--font-coinbase-sans)'],
         text: ['var(--font-coinbase-text)'],
+      },
+      backgroundImage: {
+        'trending-linear-gradient':
+          'linear-gradient(89.61deg, #42A4FF 0.34%, #A462F7 25.17%, #FF58A6 50.01%, #FF833D 74.84%, #FFDD2C 99.68%)',
+        'trending-linear-gradient-image':
+          'url(/trending/trending-gradient.png)',
       },
     },
   },
