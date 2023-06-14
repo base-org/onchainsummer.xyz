4f892bdee494cabf14afa4dec762d043c4399c86
lauchness <lauchlanchisholm@gmail.com>
2023-06-14 12:48:33 -0300

    feat: add custom fonts, colors, and box shadow to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'timer-active': '#26D356',
+        'primary-button-gradient':
+          'linear-gradient(white, white) padding-box, linear-gradient(to right, darkblue, darkorchid) border-box',
+        'secondary-button-gradient':
+          'linear-gradient(180deg, rgba(223, 225, 231, 0) 0%, rgba(223, 225, 231, 0.15) 100%), #FFFFFF',
+      },
+      boxShadow: {
+        card: '6px 6px 0px #010101',
+        button:
+          '0px 1px 2px rgba(0, 0, 0, 0.12), 0px 0px 0px rgba(0, 0, 0, 0.08)',
+      },
       fontFamily: {
-        sans: ['var(--font-satoshi)'],
-        mono: ['var(--font-dm-mono)'],
+        sans: ['var(--font-coinbase-sans)'],
+        text: ['var(--font-coinbase-text)'],
       },
     },
   },
