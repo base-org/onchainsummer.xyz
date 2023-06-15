4b01044710e4d7db823d6bedd3b13f0d1f6a77a2
lauchness <lauchlanchisholm@gmail.com>
2023-06-15 16:30:37 -0300

    feat: update button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
+import { Button } from '@/components/Button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-between">
-      <h1>No Party Like an S-Club Party</h1>
-      <Image
-        src="/pikachu.png"
-        alt="Pikachu doing Pikachu"
-        className="dark:invert"
-        width={400}
-        height={24}
-        priority
-      />
+      <h1>Peep these buttons</h1>
+      <div className="h-full flex items-center gap-20">
+        <div className="flex flex-col items-center gap-10">
+          <Button variant="PRIMARY">Mint NFT</Button>
+          <Button variant="PRIMARY" size="SMALL">
+            Mint NFT
+          </Button>
+        </div>
+        <div className="flex flex-col items-center gap-10">
+          <Button variant="SECONDARY">Mint NFT</Button>
+          <Button variant="SECONDARY" size="SMALL">
+            Mint NFT
+          </Button>
+        </div>
+        <div className="flex flex-col items-center gap-10">
+          <Button variant="TERTIARY">Mint NFT</Button>
+          <Button variant="TERTIARY" size="SMALL">
+            Mint NFT
+          </Button>
+        </div>
+      </div>
     </main>
   )
 }
