f33d65059dfe460dc95c2b48fcd2a1b879b0371e
lauchness <lauchlanchisholm@gmail.com>
2023-06-16 14:05:34 -0300

    feat: extract partner hero to reusable component, style as per design

--- src/app/partners/[slug]/page.tsx
+++ src/app/partners/[slug]/page.tsx
@@ -1,15 +1,7 @@
 import { notFound } from 'next/navigation'
-import { Partner, partners } from '@/config/partners'
-import Image from 'next/image'
-import { Card } from '../../../components/Card'
-import clsx from 'clsx'
-import { Countdown } from '@/components/Countdown'
-import { Separator } from '@/components/Separator'
-import { Button } from '@/components/Button'
+import { partners } from '@/config/partners'
 
-interface PageProps {
-  partner: Partner
-}
+import { PartnerHero } from '@/components/PartnerHero/PartnerHero'
 
 const Page = async ({ params }: { params: { slug: string } }) => {
   const slug = params.slug
@@ -19,79 +11,9 @@ const Page = async ({ params }: { params: { slug: string } }) => {
     notFound()
   }
 
-  const { name, drop, url } = partner
-
   return (
-    <main className="mt-11 px-20 py-10">
-      <h1 className="font-medium text-[40px] leading-[50px]">
-        Onchain Art From{' '}
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#9360FF] from-5% via-[#FF912C] to-[#FFEA2D] to-95%">
-          {name}
-        </span>
-      </h1>
-      <section className="flex gap-20 mt-7 pr-20">
-        <Image
-          src={`/partners/${slug}/main.png`}
-          alt={`Cover image for '${drop.name}' by ${name}`}
-          width={445}
-          height={445}
-        />
-        <div className="flex flex-col gap-6 w-full">
-          <div className="flex gap-6 items-center">
-            <Card>
-              <div className="p-2">
-                <Image
-                  src={`/partners/${slug}/icon.png`}
-                  alt={`${name} icon`}
-                  width={32}
-                  height={32}
-                />
-              </div>
-            </Card>
-            <h3 className="text-neutral-500 font-medium text-xl leading-none font-text tracking-[-0.01em]">
-              Collection by{' '}
-              <a href={url} rel="noopener" className="underline">
-                {name}
-              </a>
-            </h3>
-          </div>
-
-          <h2 className="font-medium text-[40px]">{drop.name}</h2>
-
-          <div className="flex gap-5 font-text">
-            <div>
-              <h4 className="text-sm text-neutral-900/50">Remaining Time</h4>
-              <div className="flex gap-2 items-center font-medium">
-                <div
-                  className={clsx('h-2 w-2 rounded-full', {
-                    'bg-timer-active': drop.endDate > new Date().getTime(),
-                    'bg-red': drop.endDate <= new Date().getTime(),
-                  })}
-                />
-                <div>
-                  <Countdown date={drop.endDate} completedText={'Drop Ended'} />
-                </div>
-              </div>
-            </div>
-            <div>
-              <h4 className="text-sm text-neutral-900/50">Price</h4>
-              <div className="font-medium">{drop.price} ETH</div>
-            </div>
-          </div>
-
-          <Separator />
-
-          <p className="leading-7 font-text">{drop.description}</p>
-
-          <Separator />
-          <div className="flex gap-6">
-            <Button className="w-full">Mint NFT</Button>
-            <Button className="w-full" variant="SECONDARY">
-              Read More
-            </Button>
-          </div>
-        </div>
-      </section>
+    <main>
+      <PartnerHero partner={partner} />
     </main>
   )
 }
---
+++ src/components/PartnerHero/PartnerHero.tsx
@@ -0,0 +1,80 @@
+import { Partner } from '@/config/partners'
+import Image from 'next/image'
+import { FC } from 'react'
+
+interface PartnerHeroProps {
+  partner: Partner
+}
+
+import classes from './style.module.css'
+import { Card } from '../Card'
+import clsx from 'clsx'
+import { Countdown } from '../Countdown'
+import { Separator } from '../Separator'
+import { Button } from '../Button'
+
+export const PartnerHero: FC<PartnerHeroProps> = ({
+  partner: { brandColor, name, description, icon, iconInverse, drop, url },
+}) => {
+  return (
+    <section className="flex flex-col relative px-6 pt-10 md:px-[120px] md:pt-[120px] overflow-x-hidden">
+      <div
+        className={classes.blur}
+        style={{
+          backgroundImage: `linear-gradient(89.61deg, ${brandColor} 0.34%, ${brandColor}90 99.68%);`,
+        }}
+      />
+      <h1 className="font-bold text-[39px] leading-[50px] text-center lg:text-[72px] lg:leading-[82px] md:text-start">
+        Onchain{' '}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#309FA7] via-[#9060FF] to-[#0052FF]">
+          Art
+        </span>{' '}
+        From{' '}
+        <span
+          className="text-transparent bg-clip-text"
+          style={{
+            backgroundImage: `linear-gradient(to right, ${brandColor} 0.34%, ${brandColor}40 99.68%)`,
+          }}
+        >
+          {name}
+        </span>
+      </h1>
+      <p className="mt-2 font-medium font-text leading-[180%] text-[#010101]/50 text-center md:text-xl md:leading-9 md:text-start">
+        {description}
+      </p>
+
+      <section className="flex flex-col gap-7 mt-10 lg:mt-20 lg:flex-row lg:gap-20">
+        <Card className="relative aspect-square w-full max-w-md">
+          <Image
+            src={drop.image}
+            alt={`Cover image for '${drop.name}' by ${name}`}
+            fill
+          />
+        </Card>
+        <div className="flex flex-col gap-6 lg:gap-8 w-full">
+          <div className="flex flex-col-reverse w-full gap-4 lg:flex-row lg:items-center lg:justify-between">
+            <h2 className="font-medium text-2xl leading-8 text-neutral-900 md:text-[40px] md:leading-none">
+              {drop.name}
+            </h2>
+            <div className="aspect-square w-14 md:w-20 relative">
+              <Image src={iconInverse} alt={`${name} icon`} fill />
+            </div>
+          </div>
+          <div className="flex gap-6 font-text lg:gap-m">
+            <div className="flex flex-col gap-0.5">
+              <h4 className="text-sm text-neutral-900/50">Created By</h4>
+              <div className="font-medium">{drop.createdBy}</div>
+            </div>
+            <div className="flex flex-col gap-0.5">
+              <h4 className="text-sm text-neutral-900/50">Price</h4>
+              <div className="font-medium">{drop.price} ETH</div>
+            </div>
+          </div>
+          <Separator />
+          <p className="text-neutral-600 font-text">{drop.description}</p>
+          <Button>Mint {drop.price} ETH</Button>
+        </div>
+      </section>
+    </section>
+  )
+}
