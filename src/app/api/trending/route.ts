68010f5e9a2f5acaef2f94ef427dc8d78dae9d70
lauchness <lauchlanchisholm@gmail.com>
2023-06-28 20:23:26 -0300

    feat: dynamic mint dot fun

--- src/app/api/trending/route.ts
+++ src/app/api/trending/route.ts
@@ -1,19 +1,21 @@
 import axios from 'axios'
 import { NextResponse } from 'next/server'
 
+const API_URL = process.env.NEXT_PUBLIC_MINT_DOT_FUN_API_URL
+const API_KEY = process.env.NEXT_PUBLIC_MINT_DOT_FUN_API_KEY
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const connectedWallet = searchParams.get('connectedWallet')
 
+  const url = `${API_URL}/collections?connectedWallet=${connectedWallet}`
+
   try {
-    const response = await axios.get(
-      `https://api.mint.fun/api/v1/collections?connectedWallet=${connectedWallet}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.MINT_FUN_API_KEY}`,
-        },
-      }
-    )
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    })
 
     return NextResponse.json(response?.data)
   } catch (error) {
