d16253c66bb3b3143b40d52f5662a4ea10bb7900
lauchness <lauchlanchisholm@gmail.com>
2023-07-12 21:20:15 -0300

    feat: extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         'ocs-black': '#151515',
         'ocs-red': '#FF2D2D',
         'ocs-blue': '#0052FF',
+        'ocs-turquoise': '#54DCE7',
       },
       boxShadow: {
         card: '6px 6px 0px #010101',
