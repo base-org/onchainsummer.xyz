abb2b16183dac62a21c0a59b188e67899f009db3
Wilson Cusack <wilson.cusack@coinbase.com>
2023-07-18 14:16:21 -0400

    feat: added more from OP tutorial, but not seeing any messages after relayed

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -17,13 +17,30 @@ export default function Bridge () {
     }, [signer])
 
     const bridge = useCallback(async () => {
+        const start = new Date()
         if (!messenger) return;
 
         const response = await messenger.depositETH(1)
         console.log(`Transaction hash (on L1): ${response.hash}`)
         await response.wait()
+        console.log("Waiting for status to change to RELAYED")
+        console.log(`Time so far ${(new Date().getSeconds() - start.getSeconds())/1000} seconds`)
+        await messenger.waitForMessageStatus(response.hash, 
+                                                        OP.MessageStatus.RELAYED) 
+
+        await reportBalances()    
+        console.log(`depositETH took ${(new Date().getSeconds()-start.getSeconds())/1000} seconds\n\n`)
 
     }, [messenger])
+
+    const reportBalances = useCallback(async () => {
+        if (!messenger) return;
+        
+        const l1Balance = (await messenger.l1Signer.getBalance()).toString().slice(0,-9)
+        const l2Balance = (await messenger.l2Signer.getBalance()).toString().slice(0,-9)
+      
+        console.log(`On L1:${l1Balance} Gwei    On L2:${l2Balance} Gwei`)
+      }, [messenger]) 
     
 
     return (
