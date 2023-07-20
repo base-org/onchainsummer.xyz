003cd1e140da69eb9622725ef9eba7cc056f4911
Djordje <djordje_v@hotmail.com>
2023-07-20 16:11:38 -0700

    feat: use partner brand conlor on partner pages

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -3,18 +3,26 @@
 import clsx from 'clsx'
 import { useNetworkMismatch } from '@thirdweb-dev/react'
 import { ReactNode } from 'react'
+import { SubNav } from '../SubNav'
 
 interface PageContainerProps {
   children: ReactNode
+  subNavBgColor?: string
 }
 
-export const PageContainer: React.FC<PageContainerProps> = ({ children }) => {
+export const PageContainer: React.FC<PageContainerProps> = ({
+  children,
+  subNavBgColor,
+}) => {
   const isMismatched = useNetworkMismatch()
   const heroMarginTop = isMismatched ? 'mt-28 md:mt-6' : '-mt-[100px] md:-mt-16'
 
   return (
-    <main className={clsx('w-full max-w-[1248px] mx-auto', heroMarginTop)}>
-      {children}
-    </main>
+    <>
+      <SubNav subNavBgColor={subNavBgColor} />
+      <main className={clsx('w-full max-w-[1248px] mx-auto', heroMarginTop)}>
+        {children}
+      </main>
+    </>
   )
 }
