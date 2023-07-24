daba834af8a135e60a6a3a39b4a2d7c66f6a627e
lauchness <lauchlanchisholm@gmail.com>
2023-07-24 11:41:23 -0300

    feat: trending page use l2

--- src/app/api/trending/route.ts
+++ src/app/api/trending/route.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { NextResponse } from 'next/server'
 import { Collection } from '@/utils/mintDotFunTypes'
+import { l2 } from '@/config/chain'
 
 const API_URL = process.env.NEXT_PUBLIC_MINT_DOT_FUN_API_URL
 const API_KEY = process.env.NEXT_PUBLIC_MINT_DOT_FUN_API_KEY
-const CHAIN = process.env.NEXT_PUBLIC_MINT_DOT_FUN_CHAIN_ID
 
 interface MediaObject {
   ethPrice: string
@@ -25,8 +25,8 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const connectedWallet = searchParams.get('connectedWallet')
 
-  const url = `${API_URL}/collections?connectedWallet=${connectedWallet}&chain=${CHAIN}`
-  1
+  const url = `${API_URL}/collections?connectedWallet=${connectedWallet}&chain=${l2.chainId}`
+
   try {
     let media: Media[] = []
 
