1c40af207b6104ae72e589d7b4b5a52fc1720e38
lauchness <lauchlanchisholm@gmail.com>
2023-07-24 23:03:14 -0300

    feat: anotherblock config stubbed out

--- src/config/partners/index.ts
+++ src/config/partners/index.ts
@@ -4,7 +4,8 @@ import sushi from './sushi'
 import coke from './coke'
 import fwb from './fwb'
 import parallel from './parallel'
+import anotherblock from './anotherblock'
 
-export const partners = [openSea, sushi, coke, fwb, parallel]
+export const partners = [openSea, sushi, coke, fwb, parallel, anotherblock]
 
-export { openSea, openSeason, sushi, coke, fwb, parallel }
+export { openSea, openSeason, sushi, coke, fwb, parallel, anotherblock }
--- src/config/schedule.ts
+++ src/config/schedule.ts
@@ -1,21 +1,17 @@
 import { isProd } from './chain'
-import { openSea, sushi, openSeason, coke, fwb, parallel } from './partners'
+import { coke, fwb, parallel, anotherblock } from './partners'
 import { Partner } from './partners/types'
 
 export const schedule: Record<string, Partner> = isProd
   ? {
-      '2023-08-09': fwb,
-      '2023-08-10': parallel,
-      '2023-08-11': coke,
+      '2023-08-10': fwb,
+      '2023-08-11': parallel,
+      '2023-08-12': coke,
+      '2023-08-13': anotherblock,
     }
   : {
       '2023-07-18': fwb,
       '2023-07-19': parallel,
       '2023-07-20': coke,
-      '2023-07-21': openSeason,
-      '2023-07-22': sushi,
-      '2023-07-23': sushi,
-      '2023-07-24': openSea,
-      '2023-07-25': openSea,
-      '2023-07-26': openSea,
+      '2023-07-21': anotherblock,
     }
