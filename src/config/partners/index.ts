05e5a4d3ba182604c3135e9e777f25eb0d12ebd2
lauchness <lauchlanchisholm@gmail.com>
2023-07-24 23:09:28 -0300

    feat: stand with crypto config stubbed out

--- src/config/partners/index.ts
+++ src/config/partners/index.ts
@@ -5,7 +5,25 @@ import coke from './coke'
 import fwb from './fwb'
 import parallel from './parallel'
 import anotherblock from './anotherblock'
+import standWithCrypto from './stand-with-crypto'
 
-export const partners = [openSea, sushi, coke, fwb, parallel, anotherblock]
+export const partners = [
+  openSea,
+  sushi,
+  coke,
+  fwb,
+  parallel,
+  anotherblock,
+  standWithCrypto,
+]
 
-export { openSea, openSeason, sushi, coke, fwb, parallel, anotherblock }
+export {
+  openSea,
+  openSeason,
+  sushi,
+  coke,
+  fwb,
+  parallel,
+  anotherblock,
+  standWithCrypto,
+}
--- src/config/schedule.ts
+++ src/config/schedule.ts
@@ -1,5 +1,5 @@
 import { isProd } from './chain'
-import { coke, fwb, parallel, anotherblock } from './partners'
+import { coke, fwb, parallel, anotherblock, standWithCrypto } from './partners'
 import { Partner } from './partners/types'
 
 export const schedule: Record<string, Partner> = isProd
@@ -8,10 +8,12 @@ export const schedule: Record<string, Partner> = isProd
       '2023-08-11': parallel,
       '2023-08-12': coke,
       '2023-08-13': anotherblock,
+      '2023-08-14': standWithCrypto,
     }
   : {
       '2023-07-18': fwb,
       '2023-07-19': parallel,
       '2023-07-20': coke,
       '2023-07-21': anotherblock,
+      '2023-07-22': standWithCrypto,
     }
