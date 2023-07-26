afbb4670fedde71423daebd50db290620b5bbac1
Djordje <djordje_v@hotmail.com>
2023-07-26 14:31:58 -0700

    chore: reduce size of icon

--- src/components/Teaser/Teaser.tsx
+++ src/components/Teaser/Teaser.tsx
@@ -10,7 +10,7 @@ export const Teaser: FC<TeaserProps> = ({}) => {
 
   return (
     <div className="flex flex-col justify-center items-center z-0 mx-4 sm:max-w-[1286px] lg:mx-auto">
-      <div className="h-[90px] w-[90px] rounded-full bg-[#FCD22D] mt-20"></div>
+      <div className="h-[54px] w-[54px] rounded-full bg-[#FCD22D] mt-20"></div>
       <h1 className="my-10 text-7xl sm:text-[120px] font-display">08.09.23</h1>
       <div className="mb-20">
         <PartnerHero
