2a6cbb93791c38e86c0bbf8b76f32e73035fc460
lauchness <lauchlanchisholm@gmail.com>
2023-07-27 00:08:14 -0300

    feat: tabs drop cards using background and icons

--- src/components/Tabs/TabsDropCard.tsx
+++ src/components/Tabs/TabsDropCard.tsx
@@ -11,6 +11,7 @@ interface TabsDropCardProps {
   image: string
   description: string
   slug: string
+  brandColor: string
   link?: boolean
 }
 
@@ -22,20 +23,25 @@ export const TabsDropCard: React.FC<TabsDropCardProps> = ({
   image,
   description,
   slug,
+  brandColor,
   link = false,
 }) => {
   return (
     <>
       <div
-        className="flex flex-col md:flex-row md:pr-6 bg-white rounded-2xl relative"
+        className="flex flex-col md:flex-row md:pr-6 bg-white rounded-2xl relative overflow-hidden"
         key={address}
       >
-        <div className="flex flex-[1_0_auto] p-4 w-full md:w-[329px]">
-          <div className="relative aspect-square my-auto w-full">
-            <Image src={image} alt={name} fill />
+        <div
+          className="p-4 w-full md:w-[349px] flex-[1_0_auto] justify-center"
+          style={{ backgroundColor: brandColor }}
+        >
+          <div className="relative aspect-square h-[240px] mx-auto">
+            <Image src={image} alt={name} fill className="object-contain" />
           </div>
         </div>
-        <div className="md:flex md:flex-col md:justify-start p-4 md:mt-0">
+
+        <div className="md:flex md:flex-col md:justify-start p-4 md:mt-0 w-full">
           <h3 className="text-sm font-mono uppercase text-[#858585] mb-3">
             {format(new Date(startDate), 'MMMM d')}
           </h3>
@@ -50,7 +56,9 @@ export const TabsDropCard: React.FC<TabsDropCardProps> = ({
             <h2 className="text-[32px] leading-8">{name}</h2>
           )}
 
-          <p className="text-neutral-600 leading-7 mt-4 mb-8">{description}</p>
+          <p className="text-neutral-600 leading-7  mt-4 mb-8 md:max-w-[540px] md:inline md:overflow-hidden md:whitespace-nowrap md:text-ellipsis">
+            {description}
+          </p>
 
           <Countdown date={endDate} startDate={startDate} />
         </div>
