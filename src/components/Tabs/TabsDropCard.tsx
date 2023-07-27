a111c1881785ac64f44d04d642cc8d8b139d8df3
lauchness <lauchlanchisholm@gmail.com>
2023-07-27 00:36:48 -0300

    feat: layout for tabs drop card images and clickable area

--- src/components/Tabs/TabsDropCard.tsx
+++ src/components/Tabs/TabsDropCard.tsx
@@ -29,25 +29,25 @@ export const TabsDropCard: React.FC<TabsDropCardProps> = ({
   return (
     <>
       <div
-        className="flex flex-col md:flex-row md:pr-6 bg-white rounded-2xl relative overflow-hidden"
+        className="flex flex-col lg:flex-row lg:pr-6 bg-white rounded-2xl relative overflow-hidden"
         key={address}
       >
         <div
-          className="p-4 w-full md:w-[349px] flex-[1_0_auto] justify-center"
+          className="p-4 w-full lg:w-[349px] flex-[1_0_auto] justify-center"
           style={{ backgroundColor: brandColor }}
         >
-          <div className="relative aspect-square h-[240px] mx-auto">
+          <div className="relative aspect-square lg:h-[240px] mx-auto">
             <Image src={image} alt={name} fill className="object-contain" />
           </div>
         </div>
 
-        <div className="md:flex md:flex-col md:justify-start p-4 md:mt-0 w-full">
+        <div className="lg:flex lg:flex-col lg:justify-start p-4 lg:mt-0 w-full">
           <h3 className="text-sm font-mono uppercase text-[#858585] mb-3">
             {format(new Date(startDate), 'MMMM d')}
           </h3>
           {link ? (
             <Link
-              className="text-[32px] leading-8after:absolute after:inset-0"
+              className="text-[32px] leading-8 after:absolute after:inset-0"
               href={`/partners/${slug}`}
             >
               {name}
@@ -56,11 +56,16 @@ export const TabsDropCard: React.FC<TabsDropCardProps> = ({
             <h2 className="text-[32px] leading-8">{name}</h2>
           )}
 
-          <p className="text-neutral-600 leading-7  mt-4 mb-8 md:max-w-[540px] md:inline md:overflow-hidden md:whitespace-nowrap md:text-ellipsis">
-            {description}
+          <p className="text-neutral-600 leading-7 mt-4">
+            {description.slice(0, 110)}
+            {description.length > 110 && '...'}
           </p>
 
-          <Countdown date={endDate} startDate={startDate} />
+          <Countdown
+            date={endDate}
+            startDate={startDate}
+            className="mt-auto pt-8"
+          />
         </div>
       </div>
     </>
