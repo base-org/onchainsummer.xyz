8c82bd38380fd986a614b8b08768b8e99c260039
Azim Ahmed <azim.ahmed7@gmail.com>
2023-07-31 13:13:51 -0400

    fix: schedule format

--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -4,13 +4,16 @@ import { schedule } from '@/config/schedule'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const modifiedSchedule = Object.keys(schedule).map((date) => ({
-    ...schedule[date],
-    drops: schedule[date].drops.map((drop) => ({
-      ...drop,
-      startDate: moment.utc(drop.startDate),
-      endDate: moment.utc(drop.endDate),
-    })),
-  }))
+  const modifiedSchedule: Record<string, any> = {}
+  Object.keys(schedule).map((date) => {
+    modifiedSchedule[date] = {
+      ...schedule[date],
+      drops: schedule[date].drops.map((drop) => ({
+        ...drop,
+        startDate: moment.utc(drop.startDate),
+        endDate: moment.utc(drop.endDate),
+      })),
+    }
+  })
   return NextResponse.json(modifiedSchedule)
 }
