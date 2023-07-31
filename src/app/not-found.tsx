3de60a89e8a565e3dce706ebbab50421970fbb50
Azim Ahmed <azim.ahmed7@gmail.com>
2023-07-31 11:18:18 -0400

    fix: not found resource styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="flex h-full flex-col items-center justify-between relative px-6 pb-36 pt-10 xl:px-0">
       <h1>Not Found</h1>
       <p>Could not find requested resource</p>
       <p>
