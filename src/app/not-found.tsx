b0a538e272c6d850b8eaeeca1c95b7d1008038e7
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-01 12:34:37 -0400

    chore: added more padding

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,8 +2,8 @@ import { Button } from '@/components/Button'
 
 export default function NotFound() {
   return (
-    <div className="flex h-full w-full justify-center items-center px-6 pt-21 pb-36 xl:px-0">
-      <div className="max-w-[374px] min-h-[152px]">
+    <div className="flex h-full w-full justify-center items-center px-6 pt-[136px] pb-[184px] xl:px-0">
+      <div className="max-w-[374px]">
         <h2 className="text-center text-[32px] mb-4">Oops, page not found!</h2>
         <p className="text-center mb-8 text-[#444444]">
           We couldn&rsquo;t find what you were looking for.
