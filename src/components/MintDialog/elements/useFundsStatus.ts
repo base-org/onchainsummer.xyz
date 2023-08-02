82ebcf6654178339ce915c92df1ca2f2f4a75463
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-02 14:16:20 -0400

    chore: moved around fetch call

--- src/components/MintDialog/elements/useFundsStatus.ts
+++ src/components/MintDialog/elements/useFundsStatus.ts
@@ -1,36 +1,29 @@
-import { useEffect, useState } from 'react'
-import { checkBalances } from './checkBalances'
-import { ModalPage } from '../types'
 import { useAccount } from 'wagmi'
+import { useEffect, useState, useCallback } from 'react'
+
+import { checkBalances } from './checkBalances'
 
 type FundsStatus = 'insufficient' | 'sufficient' | 'bridge'
 
-export const useFundsStatus = (
-  totalPrice: string,
-  open: boolean,
-  page?: ModalPage
-) => {
-  const {address} = useAccount()
+export const useFundsStatus = (totalPrice: string) => {
+  const { address } = useAccount()
 
   const [fundsStatus, setFundsStatus] = useState<FundsStatus>('sufficient')
 
+  const getFundsStatus = useCallback(async () => {
+    if (!address) return
+
+    const status = await checkBalances(address, totalPrice)
+
+    setFundsStatus(status)
+  }, [address, totalPrice])
+
   useEffect(() => {
-    const getFundsStatus = async () => {
-      if (
-        !address ||
-        !open ||
-        (page !== ModalPage.NATIVE_MINT && page !== ModalPage.BRIDGE_SUCCESS)
-      )
-        return
-
-      const status = await checkBalances(address, totalPrice)
-
-      setFundsStatus(status)
-    }
     getFundsStatus()
-  }, [address, open, totalPrice, page])
+  }, [address, getFundsStatus, totalPrice])
 
   return {
     fundsStatus,
+    getFundsStatus,
   }
 }
