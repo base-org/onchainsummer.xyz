887cabcf147c01cc26a7f0002d5e47c6c42f718f
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-02 21:45:04 -0400

    chore: insufficient fund without CC flow

--- src/components/MintDialog/pages/InsufficientFunds/InsufficientFunds.tsx
+++ src/components/MintDialog/pages/InsufficientFunds/InsufficientFunds.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, FunctionComponent, useState } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import dialogClasses from '@/components/dialog.module.css'
 import useBalances from '@/utils/useBalances'
@@ -9,6 +9,7 @@ import { Copy } from '@/components/icons/Copy'
 import { Checkmark } from '@/components/icons/Checkmark'
 import { usePollBalance } from './usePollBalance'
 import { useAccount } from 'wagmi'
+import { useMintDialogContext } from '../../Context/useMintDialogContext'
 
 interface InsufficientFundsProps {
   minimalBalance: string
@@ -16,15 +17,76 @@ interface InsufficientFundsProps {
   totalPrice: string
 }
 
+const WalletAddressCopy: FunctionComponent = () => {
+  const { l2Balance } = useBalances()
+  const { address } = useAccount()
+  const [isCopied, setIsCopied] = useState(false)
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between text-button-text-text leading-none">
+        <span className="font-medium text-ocs-black">Your wallet</span>
+        <span>{formatEther(l2Balance)} ETH</span>
+      </div>
+      <button
+        className="border border-[#EFEFEF] bg-white hover:bg-[#EFEFEF] p-3 rounded-[100px] text-button-text-text relative pr-12 whitespace-nowrap overflow-hidden text-ellipsis"
+        onClick={() => {
+          if (!address || !navigator) return
+          navigator.clipboard.writeText(address)
+
+          setIsCopied(true)
+        }}
+      >
+        {address}
+        {isCopied ? (
+          <Checkmark className="absolute top-1/2 right-5 transform -translate-y-1/2" />
+        ) : (
+          <Copy className="absolute top-1/2 right-5 transform -translate-y-1/2" />
+        )}
+      </button>
+    </div>
+  )
+}
+
 export const InsufficientFunds: FC<InsufficientFundsProps> = ({
   minimalBalance,
   setPage,
   totalPrice,
 }) => {
-  const { l2Balance } = useBalances()
-  const {address} = useAccount()
-  const [isCopied, setIsCopied] = useState(false)
+  const { crossMintClientId } = useMintDialogContext()
   usePollBalance(setPage, totalPrice)
+
+  if (!crossMintClientId) {
+    return (
+      <div className="flex flex-col md:my-auto gap-6">
+        <Dialog.Title className={dialogClasses.title}>
+          You need some ETH to mint
+        </Dialog.Title>
+
+        <div className={'flex flex-col w-full gap-6 md:gap-8'}>
+          <Dialog.Description className="flex flex-col w-full gap-4">
+            <span>
+              To mint “Bridge to Base”, you need ETH on Base network. Send ETH
+              to your address now, or tap “How to buy ETH” if you need help.
+            </span>
+          </Dialog.Description>
+          <WalletAddressCopy />
+          <div className="flex flex-col gap-4">
+            <Button href="https://help.coinbase.com/en/coinbase/trading-and-funding/buying-selling-or-converting-crypto/how-do-i-buy-digital-currency">
+              HOW TO BUY ETH
+            </Button>
+            <Button
+              variant="LIGHT"
+              onClick={() => setPage(ModalPage.NATIVE_MINT)}
+            >
+              Back
+            </Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-col md:my-auto gap-6">
       <Dialog.Title className={dialogClasses.title}>
@@ -38,29 +100,7 @@ export const InsufficientFunds: FC<InsufficientFundsProps> = ({
             ETH to your Base address.
           </span>
         </Dialog.Description>
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-between text-button-text-text leading-none">
-            <span className="font-medium text-ocs-black">Your wallet</span>
-            <span>{formatEther(l2Balance)} ETH</span>
-          </div>
-          <button
-            className="border border-[#EFEFEF] bg-white hover:bg-[#EFEFEF] p-3 rounded-[100px] text-button-text-text relative pr-12 whitespace-nowrap overflow-hidden text-ellipsis"
-            onClick={() => {
-              if (!address || !navigator) return
-              navigator.clipboard.writeText(address)
-
-              setIsCopied(true)
-            }}
-          >
-            {address}
-            {isCopied ? (
-              <Checkmark className="absolute top-1/2 right-5 transform -translate-y-1/2" />
-            ) : (
-              <Copy className="absolute top-1/2 right-5 transform -translate-y-1/2" />
-            )}
-          </button>
-          {}
-        </div>
+        <WalletAddressCopy />
         <div className="flex flex-col gap-4">
           <Button onClick={() => setPage(ModalPage.CROSS_MINT_FORM)}>
             MINT WITH CREDIT CARD
