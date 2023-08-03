d93d46e63a93114e65dcd8693ce946b7f85ac1eb
Wilson Cusack <wilson.cusack@coinbase.com>
2023-08-03 07:05:10 -0700

    fix: bump l1GasToBridge to 0.001

--- src/components/MintDialog/elements/getGasData.ts
+++ src/components/MintDialog/elements/getGasData.ts
@@ -3,6 +3,6 @@ import { parseEther } from 'ethers/lib/utils'
 export const getGasData = async () => {
   return {
     l2GasEstimate: parseEther('0.0006'),
-    l1GasToBridge: parseEther('0.01'),
+    l1GasToBridge: parseEther('0.001'),
   }
 }
