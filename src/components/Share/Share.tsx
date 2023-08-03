06736d5bb1e6cfdacc1c5d88357b1cd1fa9e201e
Arpit Srivastava <arpit.srivastava@coinbase.com>
2023-08-03 18:04:34 +0530

    feat: add lens
    Signed-off-by: Arpit Srivastava <arpit.srivastava@coinbase.com>

--- src/components/Share/Share.tsx
+++ src/components/Share/Share.tsx
@@ -5,9 +5,11 @@ import { TwitterIcon } from '@/components/icons/Twitter';
 import { useMintDialogContext } from '@/components/MintDialog/Context/useMintDialogContext';
 import { website } from '@/config/website';
 import { WarpCast } from '@/components/icons/Warpcast';
+import {Lens} from "@/components/icons/Lens";
 
 const twitterURL: string = "https://twitter.com/intent/tweet";
 const warpCastURL: string = "https://warpcast.com/";
+const lensURL: string = "https://lenster.xyz/";
 
 type ShareComponentProps = {}
 export const Share:  FC<ShareComponentProps> = () => {
@@ -16,6 +18,7 @@ export const Share:  FC<ShareComponentProps> = () => {
 
     const tweetUrl = `${twitterURL}?url=${website.url}&text=${shareText}`;
     const warpCastShareUrl = `${warpCastURL}/~/compose?text=${shareText}&embeds[]=${website.url}`;
+    const lensShareUrl = `${lensURL}?text=${shareText}&url=${website.url}`;
 
     return (
         <div className="flex flex-row items-center m-[10px] gap-4">
@@ -25,10 +28,18 @@ export const Share:  FC<ShareComponentProps> = () => {
                     <p className="text-sm font-mono uppercase text-[#858585] mt-[6px]">Twitter</p>
                 </Link>
             </div>
+
             <div className="flex flex-col items-center space-x-2">
                 <Link href={warpCastShareUrl} target="_blank" className="flex flex-col items-center">
                     <WarpCast/>
                     <p className="text-sm font-mono uppercase text-[#858585] mt-[6px]">Warpcast</p>
+                </Link>
+            </div>
+
+            <div className="flex flex-col items-center space-x-2">
+                <Link href={lensShareUrl} target="_blank" className="flex flex-col items-center">
+                    <Lens/>
+                    <p className="text-sm font-mono uppercase text-[#858585] mt-[6px]">Lens</p>
                 </Link>
             </div>
         </div>
