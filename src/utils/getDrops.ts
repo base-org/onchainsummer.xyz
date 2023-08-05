c7f5b8388b95b58861450eca7195fbcf6f685ff7
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-04 20:16:48 -0400

    chore: random feature removed, first NFT to become the feature NFT of the collection

--- src/utils/getDrops.ts
+++ src/utils/getDrops.ts
@@ -4,8 +4,8 @@ import { randomIntFromInterval } from './randomIntFromInterval'
 export function getDrops(drops: Drop[], dropAddress?: string) {
   const dropsLength = drops.length
 
-  const randomDropIndex = randomIntFromInterval(0, dropsLength - 1)
-  const randomDrop = drops[randomDropIndex]
+  // const randomDropIndex = randomIntFromInterval(0, dropsLength - 1)
+  const randomDrop = drops[0]
 
   const featuredDrop = dropAddress
     ? drops.find((drop) => drop.address === dropAddress) || randomDrop
