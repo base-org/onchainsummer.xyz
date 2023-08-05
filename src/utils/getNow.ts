e1475a6c6aab6271759118dc1de9cad40cfa4d02
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-04 21:22:25 -0400

    chore: default spoofing date param

--- src/utils/getNow.ts
+++ src/utils/getNow.ts
@@ -1,7 +1,8 @@
 import isValid from 'date-fns/isValid'
-const ALLOW_SPOOFING = process.env.NEXT_PUBLIC_ALLOW_SPOOFING === 'true'
+const defaultSpoofDate = process.env.NEXT_PUBLIC_SPOOF_DATE
+const ALLOW_SPOOFING = defaultSpoofDate !== undefined
 
-export const getNow = (spoofDate?: string | null) => {
+export const getNow = (spoofDate: string = defaultSpoofDate || '') => {
   const nowEST = new Date().getTime() - 4 * 60 * 60 * 1000
 
   if (!ALLOW_SPOOFING) {
