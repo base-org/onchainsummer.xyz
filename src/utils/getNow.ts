9ff3a469bffa5acb439973bf2b40a558c992f336
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-04 21:25:57 -0400

    chore: fix build issue

--- src/utils/getNow.ts
+++ src/utils/getNow.ts
@@ -2,7 +2,7 @@ import isValid from 'date-fns/isValid'
 const defaultSpoofDate = process.env.NEXT_PUBLIC_SPOOF_DATE
 const ALLOW_SPOOFING = defaultSpoofDate !== undefined
 
-export const getNow = (spoofDate: string = defaultSpoofDate || '') => {
+export const getNow = (spoofDate: string | null = defaultSpoofDate || '') => {
   const nowEST = new Date().getTime() - 4 * 60 * 60 * 1000
 
   if (!ALLOW_SPOOFING) {
