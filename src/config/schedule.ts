83b6d9f10ebe15e5fe82fed197e9a7f56b3263db
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-07 18:40:32 -0400

    chore: synced schedule dates

--- src/config/schedule.ts
+++ src/config/schedule.ts
@@ -1,5 +1,5 @@
-import { isProd } from './chain'
 import {
+  base,
   fwb,
   parallel,
   coke,
@@ -27,19 +27,20 @@ import {
 import { Partner } from './partners/types'
 
 export const schedule: Record<string, Partner> = {
+  '2023-08-09': base,
   '2023-08-10': fwb,
   '2023-08-11': parallel,
-  '2023-08-12': coke,
-  '2023-08-13': anotherblock,
+  '2023-08-12': anotherblock,
+  '2023-08-13': coke,
   '2023-08-14': standWithCrypto,
-  '2023-08-15': highlight,
+  '2023-08-15': blackbird,
   '2023-08-16': zora,
   '2023-08-17': pixelmon,
   '2023-08-18': showtime,
   '2023-08-19': mirrorWellness,
   '2023-08-20': indelible,
   '2023-08-21': manifold,
-  '2023-08-22': blackbird,
+  '2023-08-22': highlight,
   '2023-08-23': nestcoin,
   '2023-08-24': optimism,
   '2023-08-25': atari,
