f0dbfcdfbcd715db5b46905e23767564c7e1ce5e
lauchness <lauchlanchisholm@gmail.com>
2023-08-07 18:00:56 -0300

    fix: spoof date from query param in getNow util

--- src/utils/getNow.ts
+++ src/utils/getNow.ts
@@ -2,8 +2,17 @@ import isValid from 'date-fns/isValid'
 const defaultSpoofDate = process.env.NEXT_PUBLIC_SPOOF_DATE
 const ALLOW_SPOOFING = defaultSpoofDate !== undefined
 
+const getSpoofDateFromParams = () => {
+  if (typeof window === 'undefined') {
+    return null
+  }
+  const urlParams = new URLSearchParams(window.location.search)
+  const spoofDate = urlParams.get('spoofDate')
+  return spoofDate
+}
+
 export const getNow = (spoofDate?: string | null) => {
-  const value = spoofDate || defaultSpoofDate
+  const value = spoofDate || getSpoofDateFromParams() || defaultSpoofDate
   const nowEST = new Date().getTime() - 4 * 60 * 60 * 1000
 
   if (!ALLOW_SPOOFING) {
