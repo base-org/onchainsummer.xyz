6982853a709f60ab6faec969cf4f242fd344ef5c
Arpit Srivastava <arpit.srivastava@coinbase.com>
2023-08-08 09:00:14 +0530

    fix: top margin for community
    Signed-off-by: Arpit Srivastava <arpit.srivastava@coinbase.com>

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -17,12 +17,12 @@ export const PageContainer: React.FC<PageContainerProps> = ({
   const isMismatched = useIsMisMatched();
 
 
-  const heroMarginTop = isMismatched ? 'mt-28 md:mt-6' : '-mt-[100px] md:-mt-16'
+  const heroMarginTop = isMismatched ? 'md:mt-6' : '-mt-[100px]'
 
   return (
     <>
       <SubNav subNavBgColor={subNavBgColor} />
-      <main className={clsx('w-full max-w-[1248px] mx-auto', heroMarginTop)}>
+      <main className={clsx('w-full max-w-[1248px] mx-auto mt-16', heroMarginTop)}>
         {children}
       </main>
     </>
