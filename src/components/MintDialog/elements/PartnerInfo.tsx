320d1e8da6f80b7be8adb3e8e650016ed0f24718
Djordje <djordje_v@hotmail.com>
2023-08-09 16:36:44 -0700

    chore: remove object top

--- src/components/MintDialog/elements/PartnerInfo.tsx
+++ src/components/MintDialog/elements/PartnerInfo.tsx
@@ -12,12 +12,7 @@ export const PartnerInfo: FC = () => {
   return (
     <div className="flex gap-2">
       <div className="relative z-20 h-6 w-6">
-        <Image
-          src={partnerIcon}
-          alt={`${partnerName} Icon`}
-          fill
-          className="!object-top"
-        />
+        <Image src={partnerIcon} alt={`${partnerName} Icon`} fill />
       </div>
       <span className="font-medium">{partnerName}</span>
     </div>
