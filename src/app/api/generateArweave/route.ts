727f4f7e1ba96ce73091b1d67fb11cbb3344f4e5
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-09 13:33:38 -0400

    chore: arweave script

---
+++ src/app/api/generateArweave/route.ts
@@ -0,0 +1,50 @@
+import { writeFile, unlink } from 'fs'
+import { NextResponse } from 'next/server'
+import { partners } from '@/config/partners'
+import { SDK } from '@/utils/graphqlSdk'
+
+type ArticleType = { content: { body: string; title: string } } | undefined
+
+const filePath = './generated/arweave.ts'
+const prefix = 'export const arweaves = '
+
+export async function GET(request: Request) {
+  const data: Record<string, ArticleType> = {}
+  for (const partner of partners) {
+    let article: ArticleType
+    if (data[partner.aarweaveDigest]) continue
+    try {
+      const digest = await SDK.GetMirrorTransactions({
+        digest: partner.aarweaveDigest,
+      })
+      console.log(partner.aarweaveDigest)
+
+      const articleId = digest?.transactions?.edges[0]?.node?.id
+
+      if (articleId) {
+        const res = await fetch(`https://arweave.net/${articleId}`)
+        article = (await res?.json()) as {
+          content: { body: string; title: string }
+        }
+      }
+    } catch {}
+    data[partner.aarweaveDigest] = article
+  }
+
+  const content = `${prefix} ${JSON.stringify(data)}`
+
+  try {
+    await new Promise((resolve, reject) =>
+      unlink(filePath, (err) => {
+        !!err ? reject(err) : resolve({})
+      })
+    )
+  } catch (ex) {}
+  await new Promise((resolve, reject) =>
+    writeFile(filePath, content, 'utf8', (err) =>
+      !!err ? reject(err) : resolve({})
+    )
+  )
+
+  return NextResponse.json(data, { status: 200 })
+}
---
+++ src/utils/getArweaves.ts
@@ -0,0 +1,5 @@
+import { arweaves } from '../../generated/arweave'
+
+export const getArweaves = async () => {
+  return arweaves
+}
