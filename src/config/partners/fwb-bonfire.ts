6ccddba0308ab893b6355fb08076e47069b7fd06
aneri18 <79129378+aneri18@users.noreply.github.com>
2023-08-09 15:30:02 -0700

    Update description for bonfire

--- src/config/partners/fwb-bonfire.ts
+++ src/config/partners/fwb-bonfire.ts
@@ -7,7 +7,7 @@ const fwbBonfire: Partner = {
   name: 'Bonfire',
   url: 'https://www.bonfiretoken.co/',
   description:
-    "Get token-gated content from artists on Bonfire's platforms that lets creative visions flourish.",
+    "Create a custom branded and web3-native home for your community with no code. Import your collections, design custom mint pages, and grow and engage your community through gated content, contests, and more.",
   brandColor: '#000000',
   icon: '/partners/fwb-bonfire/icon.png',
   banner: '/partners/fwb-bonfire/banner-icon.svg',
