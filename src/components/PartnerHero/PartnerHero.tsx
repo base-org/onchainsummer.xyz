fe30c561db4ae4c8f34238498bb8b3d371b013f3
lauchness <lauchlanchisholm@gmail.com>
2023-08-09 20:31:26 -0300

    fix: nft image should always be aspect-square

--- src/components/PartnerHero/PartnerHero.tsx
+++ src/components/PartnerHero/PartnerHero.tsx
@@ -30,7 +30,7 @@ export const PartnerHero: FC<PartnerHeroProps> = ({
 }) => {
   return (
     <section className="grid p-5 md:p-6 rounded-3xl md:rounded-[32px] bg-white shadow-large w-full md:grid-cols-[5fr,7fr] lg:grid-cols-[456px,1fr] gap-5 md:gap-10">
-      <div className="relative w-full aspect-square mb-1 lg:mb-0 order-1 md:order-2 xl:aspect-[7/5]">
+      <div className="relative w-full aspect-square mb-1 lg:mb-0 order-1 md:order-2">
         <Image
           src={headline.image}
           alt={headline.name}
