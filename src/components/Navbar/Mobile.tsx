9d5efb73c89858390483e4a4104708da7d65f7a5
lauchness <lauchlanchisholm@gmail.com>
2023-08-09 00:39:38 -0300

    feat: mobile nav fixed

--- src/components/Navbar/Mobile.tsx
+++ src/components/Navbar/Mobile.tsx
@@ -1,39 +1,30 @@
 'use client'
 
-import { useState } from 'react'
 import clsx from 'clsx'
 import { FC } from 'react'
 import * as NavigationMenu from '@radix-ui/react-navigation-menu'
+import * as Dialog from '@radix-ui/react-dialog'
 import { YellowDot } from '../icons/YellowDot'
 import { OCS } from '../icons/OCS'
 import { Close } from '../icons/Close'
 import Link from 'next/link'
 import { Hamburger } from '../icons/Hamburger'
-import { NavListItems } from './MobileNavListItems'
 import { Separator } from '../Separator'
+import { NavListItems } from './MobileNavListItems'
+import dialogClasses from '@/components/dialog.module.css'
+import { useAccount } from 'wagmi'
+import { ConnectDialog } from '../ConnectDialog'
+import { WalletContent } from '../WalletDialog/WalletContent'
 
 type MobileProps = {}
 
 export const Mobile: FC<MobileProps> = ({}) => {
-  const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false)
-
-  const toggleHamburgerMenu = () => {
-    setHamburgerMenuOpen(!hamburgerMenuOpen)
-  }
-
+  const { address } = useAccount()
   return (
     <NavigationMenu.Root
-      className={clsx(
-        'z-40 flex w-screen lg:hidden [&>div]:w-full',
-        hamburgerMenuOpen ? 'fixed bg-white' : 'relative'
-      )}
+      className={'z-40 flex w-screen lg:hidden [&>div]:w-full'}
     >
-      <NavigationMenu.List
-        className={clsx(
-          'flex justify-between px-6 py-3 w-full items-center',
-          hamburgerMenuOpen && 'pb-[40px]'
-        )}
-      >
+      <NavigationMenu.List className="flex justify-between px-6 py-3 w-full items-center">
         <NavigationMenu.Link asChild>
           <Link href="/">
             <YellowDot />
@@ -47,7 +38,46 @@ export const Mobile: FC<MobileProps> = ({}) => {
         </NavigationMenu.Link>
 
         <NavigationMenu.Item className="flex items-center ">
-          <NavigationMenu.Trigger
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <button className="z-40" aria-label="open menu">
+                <Hamburger />
+                <span className="sr-only">Open Menu</span>
+              </button>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className={dialogClasses.overlay} />
+              <Dialog.Content
+                className={clsx(
+                  'fixed left-0 top-0 right-0 bg-white focus:outline-none z-40 h-max max-h-screen overflow-auto'
+                )}
+              >
+                <div className="flex justify-between px-6 py-3 w-full items-center">
+                  <YellowDot />
+                  <OCS />
+                  <Dialog.Close asChild>
+                    <button aria-label="Close">
+                      <Close height={24} width={24} />
+                      <span className="sr-only">Close</span>
+                    </button>
+                  </Dialog.Close>
+                </div>
+                <Separator />
+
+                <NavListItems />
+
+                <Separator />
+                {!address ? (
+                  <div className="p-6">
+                    <ConnectDialog inNavbar title="Connect" />
+                  </div>
+                ) : (
+                  <WalletContent />
+                )}
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+          {/* <NavigationMenu.Trigger
             className="group z-40 relative"
             onClick={toggleHamburgerMenu}
           >
@@ -75,7 +105,7 @@ export const Mobile: FC<MobileProps> = ({}) => {
                 <NavListItems />
               </NavigationMenu.List>
             </NavigationMenu.Sub>
-          </NavigationMenu.Content>
+          </NavigationMenu.Content> */}
         </NavigationMenu.Item>
       </NavigationMenu.List>
     </NavigationMenu.Root>
