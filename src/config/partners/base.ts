ab096920528b86795d1df1cedaf92335d711819f
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-08 22:23:13 -0400

    chore: dummy arweave ID for testing

--- src/config/partners/base.ts
+++ src/config/partners/base.ts
@@ -18,7 +18,7 @@ const base: Partner = {
   brandColor: 'rgb(209,66,65)',
   icon: '/partners/base/icon.png',
   banner: '/partners/base/banner-icon.svg',
-  aarweaveDigest: '',
+  aarweaveDigest: 'h028XVTdP7QtZZfWAHuJ9OYmMNs0Fyi4e4fJZH_5ik4',
   twitter: '@BuildonBase',
   drops: [
     {
