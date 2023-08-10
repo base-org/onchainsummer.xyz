9b516cd7d7d2b4584ab0488fdb5003ca351c90c5
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-10 15:53:52 -0400

    chore: anotherblock partner update without assets

--- src/config/partners/anotherblock.ts
+++ src/config/partners/anotherblock.ts
@@ -1,7 +1,7 @@
 import { Partner } from './types'
-import { unlimited } from '../test-contracts'
 
 import { CAMPAIGN_HOUR, CAMPAIGN_MINUTE } from '@/config/constants'
+import { MintType } from '@/components/MintDialog/types'
 
 const anotherblock: Partner = {
   slug: 'anotherblock',
@@ -16,14 +16,49 @@ const anotherblock: Partner = {
   twitter: '@anotherblock_io',
   drops: [
     {
-      image: '/partners/anotherblock/drops/drop.webp',
-      creator: '0xd365Ae104DA3E86EA36f268050D6e5212a42e360',
-      name: 'Anotherblock Drop',
-      ...unlimited,
-      type: 'erc-721',
       startDate: Date.UTC(2023, 7, 12, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
       endDate: Date.UTC(2023, 7, 13, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
-      price: '0.0001',
+      price: '0.0022',
+      externalLink: 'https://anotherblock.io/drops/boys-noize-ich-r-u',
+      mintType: MintType.External,
+      address: '0x8b2937eAD425CCC91Fc0ad884638Dc2129c51CB2',
+      crossMintClientId: '',
+      name: 'BOYS NOIZE - ICH R U',
+      description:
+        'ALEX RIDHA aka BOYS NOIZE, the renowned DJ and artist from berlin, has been pushing the boundaries of electronic music for over two decades. with a unique fusion of techno, house, and electro, his tracks are a powerful blend of driving beats, distorted synths, and pulsating basslines that ignite dance floors worldwide. not only is BOYS NOIZE a master of production, but his legendary DJ sets skillfully blend genres and leave crowds in a state of euphoria, earning him critical acclaim and a devoted fan base.  what sets BOYS NOIZE apart is his relentless pursuit of experimentation and innovation. he constantly pushes the boundaries of his own sound, incorporating elements of punk, hip-hop, and other genres into his music. this fearless approach, which has led him to collaborate with legends such as JUSTICE, DAFT PUNK, and SKRILLEX, has earned BOYS NOIZE a well-deserved reputation as an icon in the electronic music scene. whether headlining major festivals or performing in underground clubs, BOYS NOIZE’s impact on the industry cannot be overstated. his ability to connect with his audience and transport them into a world of sonic ecstasy is what makes him a true visionary.  running his own independent label BOYSNOIZE RECORDS since 2004. which quickly evolved into a playground for discovering and cultivating new talent. The Berlin-based “non-conformist DIY anarchist collective” (Dazed & Confused) is home to an exceptional and diverse roster of future-facing artists including Locked Club, Safety Trance, Baseck, Pilo, Unklevon, Taube, Peaches and part of VIRGIL ABLOH the OFF-WHITE mastermind first offical song “ORYNGE;” co-created in BOYS NOIZE studio.',
+      creator: '0x2BBC44D4449e947d3612C2b16147d9206eaeD990',
+      image: '/partners/anotherblock/drops/drop.webp',
+      type: 'erc-721',
+    },
+    {
+      startDate: Date.UTC(2023, 7, 12, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      endDate: Date.UTC(2023, 7, 13, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      price: '0.0011',
+      externalLink: 'https://anotherblock.io/drops/laidback-luke-originator',
+      mintType: MintType.External,
+      address: '0x33eD5107f821bb1465da30B7dCe4Fb7105B0Ad16',
+      crossMintClientId: '',
+      name: 'LAIDBACK LUKE - ORIGINATOR',
+      description:
+        "CELEBRATING OUR ORIGIN W/ LAIDBACK LUKE’S “ORIGINATOR” on the 19th of august 2022, anotherblock launched with WEEKEND ON A TUESDAY by R3HAB & LAIDBACK LUKE. to celebrate the genesis drop of anotherblock, LAIDBACK LUKE is back with the unreleased song, ORIGINATOR, exclusively available for fans and the community.  LAIDBACK LUKE, the revered DJ and producer, stands as a true icon in the electronic music realm. with a career spanning over three decades, he has not only carved his own path but has also played a significant role in shaping the industry. from his origins in the house and techno underground to his current eclectic style, LAIDBACK LUKE has always been a leading light in music of the present day as well as the future. the dutch hero continues to enjoy a hugely varied and consistently noteworthy career in the game.  in 2023, the dance floors are still alive with the sound of one of LAIDBACK LUKE's most impactful collaborations, SHOW ME LOVE. the original track has since been remixed by some of the biggest names in electronic music, including VINTAGE CULTURE, DUBDOGZ and HARDWELL. each with its own unique flavor. but they all share the same infectious energy to take over the dance floor. the track’s power is a testament to LAIDBACK LUKE's talent as a producer and DJ. recent times have seen him continue his dancefloor domination by producing a slew of killer tracks with both the world’s biggest names and newcomers alike, including STEVE AOKI, SUNNERY JAMES & RYAN MARCIANO, DUBDOGZ and VINNE.  beyond his collaborations, LAIDBACK LUKE is committed to discovering and nurturing new talent. he played a pivotal role in catapulting the late AVICII and numerous other artists such as AFROJACK, STEVE ANGELLO and BINGO PLAYERS. with a reputation as a serious talent scout and for being one of the nicest guys around, he has built himself a mightily impressive musical empire that has touched the highest reaches of the music industry.",
+      creator: '0x2BBC44D4449e947d3612C2b16147d9206eaeD990',
+      image: '/partners/anotherblock/drops/drop.webp',
+      type: 'erc-721',
+    },
+    {
+      startDate: Date.UTC(2023, 7, 12, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      endDate: Date.UTC(2023, 7, 13, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      price: '0.00055',
+      externalLink: 'https://anotherblock.io/drops/kleerup-paradise',
+      mintType: MintType.External,
+      address: '0x0c664A85521d5721bca047d29238d9E2a9E9e861',
+      crossMintClientId: '',
+      name: 'KLEERUP - PARADISE',
+      description:
+        'the long-awaited new chapter of KLEERUP’s artistic journey. KLEERUP is a mastermind behind behind the nordic electronic/pop music scene, known for his melodic anthems together with artists such as ROBYN. collectors unlock a a never-before-heard teaser of his upcoming release IMAGINE PARADISE. the teaser will also work as a pre-access pass to buy ownership in his upcoming single.   some lucky holders might also get the chance to make an official remix of the song. stay tuned for more info.',
+      creator: '0x2BBC44D4449e947d3612C2b16147d9206eaeD990',
+      image: '/partners/anotherblock/drops/drop.webp',
+      type: 'erc-721',
     },
   ],
 }
