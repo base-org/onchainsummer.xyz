b00f7910a4eb3a3820b2fb949f19247665e5d53f
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-10 13:43:13 -0400

    chore: hardcoded onchainsummer.xyz as a default if VERCEL_URL is missing

--- src/utils/getURL.ts
+++ src/utils/getURL.ts
@@ -1,4 +1,4 @@
 export const getURL = () => {
-  const url = process.env.VERCEL_URL as string
+  const url = (process.env.VERCEL_URL as string) || 'onchainsummer.xyz'
   return url.startsWith('http') ? url : `https://${url}`
 }
