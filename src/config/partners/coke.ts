4a3c18f3d564cdbb5d2b02c40db3e4ce848db0f4
AshishYelekar <ayelekar@gmail.com>
2023-08-10 23:18:42 -0400

    chore: update Coke drop names

--- src/config/partners/coke.ts
+++ src/config/partners/coke.ts
@@ -21,7 +21,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0x1DdB721BF79d3Ad33fBac72E5dEcf2A436CB42a3',
       crossMintClientId: '',
-      name: 'Vermeer',
+      name: 'Vermeer, "Girl with a Pearl Earring"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0xfefde6f946fa3D2FF8563c27bB2F89c16e4a254d',
@@ -35,7 +35,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0x916555Cd5F02E159b84d5247F8660531A4525d2d',
       crossMintClientId: '',
-      name: 'Aket',
+      name: 'Aket, "Divine Idyll"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0x43F665138A6005475ec38e52bE74679846E576A9',
@@ -49,7 +49,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0x32cF27F2753e90948195b64EC55C486EE640cF61',
       crossMintClientId: '',
-      name: 'Wonder Buhle',
+      name: 'Wonder Buhle, "You Can\'t Curse Me"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0x4a4782970516458A1EF8854Fc7d06eacAAEDfa50',
@@ -63,7 +63,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0xe4c3AA3978B61431C070109629a687bc42D66d8f',
       crossMintClientId: '',
-      name: 'Fatma Ramadan',
+      name: 'Fatma Ramadan, "The Blow Dryer"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0x7766c17d3372EdDB9093a5018E315B31808600a3',
@@ -77,7 +77,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0x766D25d7005e6B690C0ab4FA1e81a2eB9B416c50',
       crossMintClientId: '',
-      name: 'Vikram Kushwah',
+      name: 'Vikram Kushwah, "Falling in Library"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0xb5094b07A6a507253Df87dceD8E849fE27aF899b',
@@ -91,7 +91,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0x19766E9DA39ecb68b85b9B4aF3B0cE4f9a4F2ECf',
       crossMintClientId: '',
-      name: 'Stefania Tejada',
+      name: 'Stefania Tejada, "Natural Encounters"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0xe28a6f362244815875B97413CA6259525485dF84',
@@ -105,7 +105,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0x9A466C55F0cFC8Ea3cEE03DE0EB94Bf35A934522',
       crossMintClientId: '',
-      name: 'Munch',
+      name: 'Munch, "The Scream"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0x7B39DB3a713A84Ca782b3288840d54d3913e77a8',
@@ -119,7 +119,7 @@ const cokeConfig: Partner = {
       mintType: MintType.ThirdWeb,
       address: '0x65b2E459f779FF4af4e4A16bc8065A0063cF8221',
       crossMintClientId: '',
-      name: 'Van Gogh',
+      name: 'Van Gogh, "Bedroom in Arles"',
       description:
         'The Masterpiece collection uniquely brings together iconic artworks along with new works from emerging talents, all intertwined with the iconic Coca-Cola bottle. Classic masterpieces like "The Scream" by Edvard Munch and "Girl with a Pearl Earring" by Johannes Vermeer find a harmonious coexistence alongside contemporary works by talented artists like Aket and Vikram Kushwah. Each of these artworks were featured works in Coca-Cola’s Masterpiece Global campaign, and now take on a new dimension as they become digital collectibles, accessible to art enthusiasts participating in Onchain Summer.',
       creator: '0x41051B6f8397435C32915C40B18809118F2E9077',
