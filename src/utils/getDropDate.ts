4e4868e58e0b9284aa59143bfa6ac946819b38ee
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-11 21:41:45 -0400

    chore: fix syncing date

--- src/utils/getDropDate.ts
+++ src/utils/getDropDate.ts
@@ -4,6 +4,7 @@ import { getNow } from './getNow'
 export const getDropDate = (spoofDate?: string | null) => {
   const now = getNow(spoofDate)
   const date = moment.tz(now, 'America/New_York')
-  const dateToReturn = date.get('hour') >= 12 ? date : date.subtract(1, 'day')
-  return dateToReturn.format('YYYY-MM-DD')
+  return date.get('hour') >= 12
+    ? date.format('YYYY-MM-DD')
+    : date.subtract(1, 'day').format('YYYY-MM-DD')
 }
