e37a4e26170b6cb79aaa122fa424cd7988b9447f
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-14 19:40:02 -0400

    chore: error logging and response to arweave endpoint

--- src/app/api/arweave/route.ts
+++ src/app/api/arweave/route.ts
@@ -10,22 +10,29 @@ export async function POST(request: NextRequest) {
     if (!digestId) return NextResponse.json({}, { status: 400 })
 
     let article: ArticleType
+    console.log(`Fetching digest ${digestId} from Arweave API...`)
     const digest = await SDK.GetMirrorTransactions({ digest: digestId })
+    console.log(`Fetch digest complete`)
 
     const articleId = digest?.transactions?.edges[0]?.node?.id
 
     if (articleId) {
+      console.log(`Fetching article ${articleId} from Arweave API...`)
       const res = await fetch(`https://arweave.net/${articleId}`, {
         // Revalidate every minute
         next: { revalidate: 1 * 60 },
       })
+      console.log(`Fetch article complete`)
       article = (await res?.json()) as {
         content: { body: string; title: string }
       }
     }
 
     return NextResponse.json(article, { status: 200 })
-  } catch {
-    return NextResponse.json({}, { status: 400 })
+  } catch (error) {
+    return NextResponse.json(
+      { message: (error as any)?.message, stack: (error as any)?.stack },
+      { status: 400 }
+    )
   }
 }
