8d2c61c06500f28602a9bf882d08f7983f5aa43a
Lauchlan Chisholm <51837850+lauchness@users.noreply.github.com>
2023-08-14 13:30:00 -0300

    fix: max-h for mint dialog, citizenship margin (#254)

--- src/components/MintDialog/MintDialog.tsx
+++ src/components/MintDialog/MintDialog.tsx
@@ -280,7 +280,7 @@ export const MintDialog: FC<{ size?: ButtonProps['size'] }> = ({ size }) => {
         <Dialog.Content
           className={clsx(
             'data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[90vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[24px] p-5 shadow-large bg-white focus:outline-none z-40 h-auto overflow-scroll',
-            'lg:max-w-5xl lg:p-16 lg:h-full lg:overflow-hidden',
+            'lg:max-w-5xl lg:p-16 lg:h-full lg:max-h-[600px] lg:overflow-hidden',
             {
               'lg:!h-auto': desktopHeightAuto,
             }
