9796a16c9a4034f846e150d089cba1c6454d2531
Wilson Cusack <wilson.cusack@coinbase.com>
2023-08-16 16:22:41 -0400

    fix: remove get signer calls

--- src/components/MintDialog/elements/getBalances.ts
+++ src/components/MintDialog/elements/getBalances.ts
@@ -3,11 +3,8 @@ import { getJsonRpcProviders } from '@/utils/getJsonRpcProviders'
 export const getBalances = async (address: string) => {
   const { l1Provider, l2Provider } = getJsonRpcProviders()
 
-  const l1Signer = l1Provider.getSigner(address)
-  const l2Signer = l2Provider.getSigner(address)
-
-  const l1Balance = await l1Signer.getBalance()
-  const l2Balance = await l2Signer.getBalance()
+  const l1Balance = await l1Provider.getBalance(address)
+  const l2Balance = await l2Provider.getBalance(address)
 
   return { l1Balance, l2Balance }
 }
