e18df3e2196b23ba2d0d151c002f7654c3d757fd
Wilson Cusack <wilson.cusack@coinbase.com>
2023-08-17 14:37:03 -0400

    fix: remove all autoconnect

--- src/components/ConnectDialog/ConnectDialog.tsx
+++ src/components/ConnectDialog/ConnectDialog.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { WindowProvider, useConnect } from 'wagmi'
 import clsx from 'clsx'
 import { Button, ButtonProps } from '../Button'
 
@@ -10,38 +9,16 @@ type ConnectDialogProps = {
   size?: ButtonProps['size']
 }
 
-declare global {
-  interface Window {
-    ethereum?: WindowProvider
-  }
-}
-
-interface CustomWindowProvider extends WindowProvider {
-  isCoinbaseBrowser?: boolean
-}
-
 export const ConnectDialog: FC<ConnectDialogProps> = ({
   title = <div className="flex gap-2.5 items-center px-3">Mint</div>,
   inNavbar = false,
   size,
 }) => {
-  const { connect, connectors } = useConnect()
-
-  useEffect(() => {
-    const ethereum = window.ethereum as CustomWindowProvider | undefined
-    if (ethereum?.isCoinbaseBrowser) {
-      connect({
-        connector: connectors.find((c) => c.name == 'Coinbase Wallet'),
-      })
-    }
-  }, [connect, connectors])
-
   return (
     <ConnectButton.Custom>
       {({
         account,
         chain,
-        openAccountModal,
         openChainModal,
         openConnectModal,
         authenticationStatus,
