d06bb146cabc887632a9af75c417bc0cd49e1c86
Azim Ahmed <azim.ahmed7@gmail.com>
2023-08-17 14:30:28 -0400

    chore: falling back on coinbaseWallet if injected is not available

--- src/utils/useConnectCBWallet.ts
+++ src/utils/useConnectCBWallet.ts
@@ -5,8 +5,11 @@ export function useConnectCBWallet() {
   const { connect, connectors } = useConnect()
 
   return useCallback(() => {
+    alert(connectors.map((c) => c.id).join(', '))
     connect({
-      connector: connectors.find((c) => c.id == 'injected'),
+      connector:
+        connectors.find((c) => c.id == 'injected') ||
+        connectors.find((c) => c.id == 'coinbaseWallet'),
     })
   }, [connect, connectors])
 }
