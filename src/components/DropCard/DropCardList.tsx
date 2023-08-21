d4de9d286a108e03d0c4fecf06f6549ca7ee561c
Lauchlan Chisholm <51837850+lauchness@users.noreply.github.com>
2023-08-21 12:13:29 -0300

    fix: add scrollbar to drop card list (#343)

--- src/components/DropCard/DropCardList.tsx
+++ src/components/DropCard/DropCardList.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react'
 
 export const DropCardList: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="overflow-auto hide-scrollbar">
+    <div className="overflow-auto">
       <div className="flex overflow-y-hidden md:overflow-x-auto w-max">
         <ul className="flex gap-4 md:gap-8 last:pr-4">{children}</ul>
       </div>
