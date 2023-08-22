0d2c2aedf2ba80f39cc5985b9705be8a244051fa
lauchness <lauchlanchisholm@gmail.com>
2023-08-22 12:19:54 -0300

    fix: schedule format modifications from api response

--- src/utils/getSchedule.ts
+++ src/utils/getSchedule.ts
@@ -1,4 +1,18 @@
+import { Partner } from '@/config/partners/types'
 import { schedule } from '@/config/schedule'
+
+const isSchedule = (schedule: any): schedule is typeof schedule => {
+  return (
+    schedule &&
+    typeof schedule === 'object' &&
+    Object.keys(schedule).every((date) => {
+      return (
+        typeof schedule[date] === 'object' &&
+        Array.isArray(schedule[date].drops)
+      )
+    })
+  )
+}
 
 export const getSchedule = async () => {
   const fetchSchedule = await fetch(
@@ -10,7 +24,23 @@ export const getSchedule = async () => {
 
   const fetchedSchedule = (await fetchSchedule.json()) as typeof schedule
 
-  const _schedule = fetchedSchedule || schedule
+  const _schedule = isSchedule(fetchedSchedule) ? fetchedSchedule : schedule
 
-  return _schedule
+  const res = Object.keys(_schedule).reduce(
+    (acc, date) => {
+      return {
+        [date.slice(0, 10)]: {
+          ..._schedule[date],
+          drops: _schedule[date].drops.map((drop) => ({
+            ...drop,
+            startDate: new Date(drop.startDate).getTime(),
+            endDate: new Date(drop.endDate).getTime(),
+          })),
+        },
+        ...acc,
+      }
+    },
+    {} as Record<string, Partner>
+  )
+  return res
 }
