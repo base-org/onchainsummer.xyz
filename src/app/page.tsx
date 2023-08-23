c104ae46cc721f497f9edc004d227168a6b74242
Lauchlan Chisholm <51837850+lauchness@users.noreply.github.com>
2023-08-23 18:01:29 -0300

    fix: only render article/remaining drops section if one of those 2 exist (#365)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,9 +33,8 @@ const Home = async ({ searchParams }: Props) => {
   const spoofDate = Array.isArray(spoofDateParam)
     ? spoofDateParam[0]
     : spoofDateParam
-  const { partner, tabs, article, tweets, activeDrops } = await getPageData(
-    spoofDate
-  )
+  const { partner, tabs, article, tweets, activeDrops } =
+    await getPageData(spoofDate)
   const { drops, name, icon } = partner
 
   const dropAddressParam = searchParams.drop
@@ -56,82 +55,87 @@ const Home = async ({ searchParams }: Props) => {
           staticHeadline={!!dropAddress}
           floorAsk={collections[featuredDrop.address.toLowerCase()]?.floorAsk}
         />
-        <section className="bg-ocs-light-gray w-full shadow-large rounded-3xl">
-          <div className="p-[20px] lg:p-4 rounded-3xl">
-            <div className="mb-4 flex gap-2">
-              <div className="relative z-20 h-[64px] w-[64px]">
-                <Image
-                  src={icon}
-                  alt={`${partner} Icon`}
-                  height={64}
-                  width={64}
-                />
-              </div>
-              <div className="flex-1 flex flex-col justify-end">
-                <h2 className="desktop-h2">{name}</h2>
-                <p className="desktop-mono uppercase text-[#444]">Collection</p>
-              </div>
-            </div>
-            {remainingDrops?.length > 0 && (
-              <div className="-mr-4 mb-4 md:mb-14">
-                <DropCardList>
-                  {remainingDrops?.map((drop) => (
-                    <DropCard
-                      {...drop}
-                      key={drop.name}
-                      partner={name}
-                      partnerIcon={icon}
-                      openSeaLink={drop.openSeaLink}
-                      interactWithNFTLink={drop.interactWithNFTLink}
-                      dataSuffix={siteDataSuffix}
-                      dropDataSuffix={drop.dataSuffix}
-                      buttonText={drop.buttonText}
-                      description={drop.description}
-                      floorAsk={
-                        collections[drop.address.toLowerCase()]?.floorAsk
-                      }
-                    />
-                  ))}
-                </DropCardList>
-              </div>
-            )}
-            {!!article?.content && (
-              <div className="flex items-start gap-12">
-                <div className="flex flex-col rounded-xl md:pr-4lg:mx-2  break-words w-full md:w-1/2">
-                  <div className="w-full">
-                    <h2 className="desktop-h2 font-display">
-                      {article.content.title}
-                    </h2>
-                  </div>
-                  <div className="w-full">
-                    <div className="prose w-full">
-                      <ReactMarkdown
-                        content={`${article.content.body.slice(0, 500)} ...`}
-                      />
-                    </div>
-                    <Button
-                      className="uppercase border border-1 border-black !bg-transparent !text-black mt-6 max-w-fit !py-2"
-                      href={`/${partner.slug}`}
-                    >
-                      Read full story
-                    </Button>
-                  </div>
-                </div>
-                <div className="hidden md:block md:w-1/2">
+        {remainingDrops?.length > 0 || article?.content ? (
+          <section className="bg-ocs-light-gray w-full shadow-large rounded-3xl">
+            <div className="p-[20px] lg:p-4 rounded-3xl">
+              <div className="mb-4 flex gap-2">
+                <div className="relative z-20 h-[64px] w-[64px]">
                   <Image
-                    src={partner.banner}
+                    src={icon}
                     alt={`${partner} Icon`}
-                    fill
-                    style={{
-                      objectFit: 'contain',
-                    }}
-                    className="!static rounded-2xl"
+                    height={64}
+                    width={64}
                   />
                 </div>
+                <div className="flex-1 flex flex-col justify-end">
+                  <h2 className="desktop-h2">{name}</h2>
+                  <p className="desktop-mono uppercase text-[#444]">
+                    Collection
+                  </p>
+                </div>
               </div>
-            )}
-          </div>
-        </section>
+              {remainingDrops?.length > 0 ? (
+                <div className="-mr-4 mb-4 md:mb-14">
+                  <DropCardList>
+                    {remainingDrops?.map((drop) => (
+                      <DropCard
+                        {...drop}
+                        key={drop.name}
+                        partner={name}
+                        partnerIcon={icon}
+                        openSeaLink={drop.openSeaLink}
+                        interactWithNFTLink={drop.interactWithNFTLink}
+                        dataSuffix={siteDataSuffix}
+                        dropDataSuffix={drop.dataSuffix}
+                        buttonText={drop.buttonText}
+                        description={drop.description}
+                        floorAsk={
+                          collections[drop.address.toLowerCase()]?.floorAsk
+                        }
+                      />
+                    ))}
+                  </DropCardList>
+                </div>
+              ) : null}
+              {!!article?.content ? (
+                <div className="flex items-start gap-12">
+                  <div className="flex flex-col rounded-xl md:pr-4lg:mx-2  break-words w-full md:w-1/2">
+                    <div className="w-full">
+                      <h2 className="desktop-h2 font-display">
+                        {article.content.title}
+                      </h2>
+                    </div>
+                    <div className="w-full">
+                      <div className="prose w-full">
+                        <ReactMarkdown
+                          content={`${article.content.body.slice(0, 500)} ...`}
+                        />
+                      </div>
+                      <Button
+                        className="uppercase border border-1 border-black !bg-transparent !text-black mt-6 max-w-fit !py-2"
+                        href={`/${partner.slug}`}
+                      >
+                        Read full story
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="hidden md:block md:w-1/2">
+                    <Image
+                      src={partner.banner}
+                      alt={`${partner} Icon`}
+                      fill
+                      style={{
+                        objectFit: 'contain',
+                      }}
+                      className="!static rounded-2xl"
+                    />
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          </section>
+        ) : null}
+
         {tweets && Array.isArray(tweets.data) && (
           <section className="bg-ocs-light-gray rounded-3xl p-4 shadow-large">
             <div className="flex justify-between mb-4">
