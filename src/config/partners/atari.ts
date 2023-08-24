a50ba8abfe3e0e6d2027c120853a5c681217eb34
Lauchlan Chisholm <51837850+lauchness@users.noreply.github.com>
2023-08-24 12:59:57 -0300

    feat: atari config review (#363)

--- src/config/partners/atari.ts
+++ src/config/partners/atari.ts
@@ -1,6 +1,7 @@
 import { Partner } from './types'
 import { unlimited } from '../test-contracts'
 import { CAMPAIGN_HOUR, CAMPAIGN_MINUTE } from '@/config/constants'
+import { MintType } from '@/components/MintDialog/types'
 
 const atari: Partner = {
   slug: 'atari',
@@ -15,14 +16,60 @@ const atari: Partner = {
   twitter: '@atari',
   drops: [
     {
-      image: '',
-      creator: '',
-      name: '',
-      ...unlimited,
+      image:
+        'https://assets.onchainsummer.xyz/Atari Coinbase Image - Scott Asbury.png',
+      creator: '0xe0B3ab982Bd971998991c7E5775C61C1aa6445b4',
+      name: 'Atari Summer Camp',
+      description: `Nobody’s hotter than Atari this summer! We're throwing it back and bringing the heat with a collection that is hotter than your first console. We’ve hand crafted a digital collectible—The Summer Camp postcard—and 3 off-chain items (real physical merch)—a fanny pack (because pockets are so 2006), a raglan t-shirt (for when you need to flex without flexing), and a trucker hat (sun’s out, scores up).`,
+      address: '0xF47A95bf421c2349de30A465a0d54F95ee79920B',
+      crossMintClientId: '5f0d1e9a-7ee6-411f-81ec-1bf73485e9f0',
+      mintType: MintType.ThirdWeb,
       type: 'erc-721',
       startDate: Date.UTC(2023, 7, 25, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
-      endDate: Date.UTC(2023, 7, 26, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
-      price: '0.0001',
+      endDate: Date.UTC(2023, 8, 2, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      price: '0.001972',
+    },
+    {
+      image:
+        'https://assets.onchainsummer.xyz/C - Camp Raglan Shirt - Scott Asbury.png',
+      creator: '0xe0B3ab982Bd971998991c7E5775C61C1aa6445b4',
+      name: 'Atari Camp Raglan Shirt',
+      description: `Raglan tee inspired by camp days where high scores met sunscreen; a nod to joystick-era wins.`,
+      address: '0x71B8d8E3c6636656e1344D393811C3E8033f013d',
+      crossMintClientId: 'bfbf9d40-6f17-494c-89ed-cf91fa3ecb27',
+      mintType: MintType.ThirdWeb,
+      type: 'erc-721',
+      startDate: Date.UTC(2023, 7, 25, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      endDate: Date.UTC(2023, 8, 2, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      price: '0.01972',
+    },
+    {
+      image:
+        'https://assets.onchainsummer.xyz/B - Camp Trucker Hat - Scott Asbury.png',
+      creator: '0xe0B3ab982Bd971998991c7E5775C61C1aa6445b4',
+      name: 'Atari Camp Trucker Hat',
+      description: `Need to hide arcade hair or block the sun? This gamer's hat is the crown you didn't know you needed.`,
+      address: '0xa82532BD91757Dfd96547c5De115574A88AFD30E',
+      crossMintClientId: '79ba2c84-6070-48a9-b9a1-501358a353e4',
+      mintType: MintType.ThirdWeb,
+      type: 'erc-721',
+      startDate: Date.UTC(2023, 7, 25, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      endDate: Date.UTC(2023, 8, 2, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      price: '0.01972',
+    },
+    {
+      image:
+        'https://assets.onchainsummer.xyz/D - Camp Fanny Pack - Scott Asbury.png',
+      creator: '0xe0B3ab982Bd971998991c7E5775C61C1aa6445b4',
+      name: 'Atari Camp Fanny Pack',
+      description: `Retro chic fanny pack for essentials. A stylish nod to gaming's golden age with subtle flair.`,
+      address: '0x6Fa44134C5a51fa5c455008B9668caA65c8437C0',
+      crossMintClientId: '277d4b77-6182-45cc-a857-23839328a0d1',
+      mintType: MintType.ThirdWeb,
+      type: 'erc-721',
+      startDate: Date.UTC(2023, 7, 25, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      endDate: Date.UTC(2023, 8, 2, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      price: '0.01972',
     },
   ],
 }
