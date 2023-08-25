43c215a8d8ee0e1e39c99d7b83cb4c17a868c340
AshishYelekar <ayelekar@gmail.com>
2023-08-25 14:20:00 -0400

    chore: update main desc, assets, and post-mint CTA

--- src/config/partners/iskra.ts
+++ src/config/partners/iskra.ts
@@ -7,7 +7,7 @@ const iskra: Partner = {
   name: 'Iskra',
   url: 'https://iskra.world/',
   description:
-    'Play and earn with onchain games where everybody wins with Iskra.',
+    'All-in-one web3 game platform that rewards players and developers through a unique community system. Services include web3 games, wallet, DEX, marketplace, and launchpad.',
   brandColor: '#007aff',
   icon: '/partners/iskra/icon.png',
   banner: '/partners/iskra/banner-icon.svg',
@@ -16,12 +16,12 @@ const iskra: Partner = {
   drops: [
     {
       image:
-        'https://assets.onchainsummer.xyz/FINALART--[NFT]_BASE_MysteryBox-TaekyungYoun.mp4',
+        'https://assets.onchainsummer.xyz/Title NFT(Mystery Box) for header (1).mp4',
       creator: '0x5C69C4141EAB0c3852eba4983B572ffBAf742278',
       name: 'ClashMon Game NFT Mystery Box',
       description: `ClashMon is one of the many accessible onchain games from the Iskra game platform that provides of digital ownership through play, trading, collecting, crafting and competition. 
 
-Players are able to build and ‘own’ their team of Clashmons for trading, collecting and fishing (resource farming). The Collectible Battle RPG comes alive with fast-paced RPG-style monster fights and high-stakes tournament rounds, but balances out the pacing with a soothing fishing mode where players can gather resources for gear or pursue other enhancements`,
+Players are able to build and ‘own’ their team of Clashmons for trading, collecting and fishing (resource farming). The Collectible Battle RPG comes alive with fast-paced RPG-style monster fights and high-stakes tournament rounds, but balances out the pacing with a soothing fishing mode where players can gather resources for gear or pursue other enhancements.`,
       address: '0x75ed58e1D029853231A9e9825F0035E0449FBAfa',
       crossMintClientId: '521f028e-b728-491a-9336-e2f5a904d3cf',
       mintType: MintType.ThirdWeb,
@@ -29,9 +29,13 @@ Players are able to build and ‘own’ their team of Clashmons for trading, col
       startDate: Date.UTC(2023, 7, 27, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
       endDate: Date.UTC(2023, 8, 3, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
       price: '0',
+      interactWithNFTLink: {
+        url: 'https://iskra.world/clashmon_reveal',
+        label: 'Reveal Now',
+      },
     },
     {
-      image: 'https://assets.onchainsummer.xyz/',
+      image: 'https://assets.onchainsummer.xyz/Box1_Learn About ClashMon.png',
       creator: '0x5C69C4141EAB0c3852eba4983B572ffBAf742278',
       name: 'Learn About Clashmon',
       description: ``,
@@ -46,7 +50,7 @@ Players are able to build and ‘own’ their team of Clashmons for trading, col
       buttonText: 'Learn More',
     },
     {
-      image: 'https://assets.onchainsummer.xyz/',
+      image: `https://assets.onchainsummer.xyz/Box2_What's inside the Mystery Box.png`,
       creator: '0x5C69C4141EAB0c3852eba4983B572ffBAf742278',
       name: 'What’s inside the Mystery Box',
       description: ``,
@@ -61,7 +65,7 @@ Players are able to build and ‘own’ their team of Clashmons for trading, col
       buttonText: 'Reveal Now',
     },
     {
-      image: 'https://assets.onchainsummer.xyz/',
+      image: 'https://assets.onchainsummer.xyz/Box3_Learn more about Iskra.png',
       creator: '0x5C69C4141EAB0c3852eba4983B572ffBAf742278',
       name: 'Learn more about Iskra',
       description: ``,
