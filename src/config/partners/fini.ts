d201e0e7d894cb13eaa5e6241ce6f0463d580309
AshishYelekar <41811693+AshishYelekar@users.noreply.github.com>
2023-08-28 23:31:08 -0400

    chore: update fini config (#405)

--- src/config/partners/fini.ts
+++ src/config/partners/fini.ts
@@ -19,16 +19,16 @@ const fini: Partner = {
       image: 'https://assets.onchainsummer.xyz/Final_Art-Fini.png',
       creator: '0x8Bdd0535D3971C3aB09C7F1e35266e047Fe972cE',
       name: 'Base Wars',
-      description: `Mint to adopt one of three Finis that respond to your activity on Base.
+      description: `Adopt one of three Finis that respond to your activity on Base.
 Kurt and his Raybot, Ray, are Basenauts locked in combat with Spike, a notorious commando in the Starlight Extinction Corps.
 The more transactions you make each week on Base, the better Kurt and Ray can defend their fleet against the onslaught of attacks. The less you transact, the more powerful Spike becomes.`,
       address: '0x34E817D631b7FB79A54638c01c03421D124E35a7',
       crossMintClientId: '',
-      mintType: MintType.ThirdWeb,
+      mintType: MintType.Zora,
       type: 'erc-721',
       startDate: Date.UTC(2023, 7, 31, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
-      endDate: Date.UTC(2023, 8, 4, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
-      price: ' 0.005',
+      endDate: Date.UTC(2023, 8, 7, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
+      price: '0.004223',
     },
   ],
 }
