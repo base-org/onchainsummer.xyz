b6265e6d89d068ea947c690b5f3985b4cbac1b87
Lukas <lukas@lsr.dev>
2023-08-29 09:14:56 -0400

    chore: wallet analytics (#408)

--- src/components/Navbar/Wallet.tsx
+++ src/components/Navbar/Wallet.tsx
@@ -1,12 +1,21 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { ConnectDialog } from '../ConnectDialog'
 import { WalletDialog } from '../WalletDialog'
 import { useAccount } from 'wagmi'
+import { useLogEvent } from '@/utils/useLogEvent'
+import { events } from '@/utils/analytics'
 
 type WalletProps = {}
 
 export const Wallet: FC<WalletProps> = ({}) => {
-  const { address } = useAccount()
+  const { address, connector } = useAccount()
+  const logEvent = useLogEvent()
+
+  useEffect(() => {
+    if (connector) {
+      logEvent?.(events.walletConnected, { walletType: connector.id })
+    }
+  }, [connector, logEvent])
 
   if (!address) {
     return <ConnectDialog inNavbar title="Connect" />
