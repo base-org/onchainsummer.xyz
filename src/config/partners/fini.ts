80ff34926b88af148367e9acfb8e7134de60761d
AshishYelekar <ayelekar@gmail.com>
2023-08-31 11:51:38 -0400

    chore: update fini crossmint id

--- src/config/partners/fini.ts
+++ src/config/partners/fini.ts
@@ -23,7 +23,7 @@ const fini: Partner = {
 Kurt and his Raybot, Ray, are Basenauts locked in combat with Spike, a notorious commando in the Starlight Extinction Corps.
 The more transactions you make each week on Base, the better Kurt and Ray can defend their fleet against the onslaught of attacks. The less you transact, the more powerful Spike becomes.`,
       address: '0x34E817D631b7FB79A54638c01c03421D124E35a7',
-      crossMintClientId: '',
+      crossMintClientId: 'f8fa889d-60f3-4fbb-845d-ee961e94b476',
       mintType: MintType.Zora,
       type: 'erc-721',
       startDate: Date.UTC(2023, 7, 31, CAMPAIGN_HOUR, CAMPAIGN_MINUTE, 0, 0),
